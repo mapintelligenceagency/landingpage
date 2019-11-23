319f1aff59a4f3eb4fcaa8296712d9f84eb6f9d8
Felix Wehnert <flashspys@gmail.com>
2019-11-23 18:53:40 +0100

    smooth scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
-import jQuery from 'jquery';
+import jQuery from 'jquery-easing';
+
 // Start of use strict
 
 const $ = jQuery;
 
 // Smooth scrolling using jQuery easing
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function scrollingHandler() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     let target = $(this.hash);
     target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
