e60ef37b996a8bd5a59faf2e09cbdbca4e39ab3d
Gero Gerke <hello@gerogerke.de>
2020-04-06 20:36:29 +0200

    :tada: Initial Commit

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+
+import '../style/main.scss';
+import 'bootstrap';
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+import './main';
+
+if (module.hot) {
+  module.hot.accept();
+}
